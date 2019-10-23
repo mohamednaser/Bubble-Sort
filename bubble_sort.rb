a56a910ea79da1c45bcc67a036d32b5aadbe66b1
Santiago <santo1996.29@gmail.com>
2019-10-23 16:58:04 -0400

    stickler styling part 2

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,31 +1,30 @@
 def bubble_sort(unsordet_array)
-  # method to sort an array useing Bubble Sort Algorithem
+  # frozen_string_literal: true
   unsordet_array.size.times do
     (0...unsordet_array.size - 1).each do |x|
       first_elment = unsordet_array[x]
       second_elment = unsordet_array[x + 1]
-
       if second_elment < first_elment
-        temp_variable = unsordet_array[x]
-        unsordet_array[x] = unsordet_array[x + 1]
-        unsordet_array[x + 1] = temp_variable
+        unsordet_array[x], unsordet_array[x + 1] = unsordet_array[x + 1], unsordet_array[x]
+        next
       end
     end
   end
-  return unsordet_array
+  unsordet_array
 end
 
 def bubble_sort_by(arr)
-  # method to sort an array useing Block aproach
+  # frozen_string_literal: true
   arr.size.times do
     (0...arr.size - 1).each do |i|
       comparison = yield(arr[i], arr[i + 1])
-      if comparison > 0
+      if comparison.positive?
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
       end
     end
   end
-  return arr
+  arr
 end
 
 p bubble_sort_by([5, 2, 1, 6, 7]) { |left, right| left - right }
+# p bubble_sort([5, 2, 1, 6, 7])
