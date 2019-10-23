95a8768e3d55fc6b5c4b136af8e3a621555615f9
Mohamed naser <nc_m.naser@hotmail.com>
2019-10-23 22:46:07 +0200

    fix other ruby cap issues

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,6 +1,7 @@
 def bubble_sort(unsordet_array)
+  # method to sort an array useing Bubble Sort Algorithem
   unsordet_array.size.times do
-    for x in 0...unsordet_array.size - 1
+    (0...unsordet_array.size - 1).each do |x|
       first_elment = unsordet_array[x]
       second_elment = unsordet_array[x + 1]
 
@@ -15,8 +16,9 @@ def bubble_sort(unsordet_array)
 end
 
 def bubble_sort_by(arr)
-  for i in 0...arr.size
-    for i in 0...arr.size - 1
+  # method to sort an array useing Block aproach
+  arr.size.times do
+    (0...arr.size - 1).each do |i|
       comparison = yield(arr[i], arr[i + 1])
       if comparison > 0
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
@@ -26,4 +28,4 @@ def bubble_sort_by(arr)
   return arr
 end
 
-p bubble_sort([5, 2, 1, 6, 7])
+p bubble_sort_by([5, 2, 1, 6, 7]) { |left, right| left - right }
